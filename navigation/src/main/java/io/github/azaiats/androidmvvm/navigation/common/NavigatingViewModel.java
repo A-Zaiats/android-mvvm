87683219efdc2ae6ff924973670e594dda35d4e8
Andrei Zaiats <andrei.zaiats@gmail.com>
2016-06-08 22:33:16 +0300

    Added navigation command.

--- navigation/src/main/java/io/github/azaiats/androidmvvm/navigation/common/NavigatingViewModel.java
+++ navigation/src/main/java/io/github/azaiats/androidmvvm/navigation/common/NavigatingViewModel.java
@@ -34,6 +34,7 @@ import io.github.azaiats.androidmvvm.core.common.BaseViewModel;
 public abstract class NavigatingViewModel<T extends Navigator> extends BaseViewModel {
 
     @Nullable protected T navigator;
+    @Nullable protected NavigationCommand<T> pendingCommand;
 
     /**
      * Setter for navigator
@@ -41,5 +42,27 @@ public abstract class NavigatingViewModel<T extends Navigator> extends BaseViewM
      */
     public void setNavigator(@Nullable T navigator) {
         this.navigator = navigator;
+        if (navigator != null && pendingCommand != null) {
+            pendingCommand.execute(navigator);
+            pendingCommand = null;
+        }
+    }
+
+    /**
+     * Executes command if {@link Navigator} is attached.
+     * Otherwise command will be saved and executed on {@link Navigator} attach.
+     *
+     * @param navigationCommand command for {@link Navigator}
+     */
+    public void executeNavigationCommand(NavigationCommand<T> navigationCommand) {
+        if (navigationCommand == null) {
+            return;
+        }
+        T vmNavigator = navigator;
+        if (vmNavigator == null) {
+            pendingCommand = navigationCommand;
+        } else {
+            navigationCommand.execute(vmNavigator);
+        }
     }
 }
